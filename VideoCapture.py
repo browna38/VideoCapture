5e781785d273ebacb617c6cc2cd713db6f408166
browna38 <ajbrown000@gmail.com>
2020-12-07 09:46:06 -0600

    fixed video capture avi file saving

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,23 +1,8 @@
-#!/usr/bin/env python
-
-# import the necessary packages
-from imutils.video import VideoStream
-from pyzbar import pyzbar
-import argparse
-import datetime
-import imutils
-import time
 import cv2
-import numpy
-
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
-	help="path to output CSV file containing barcodes")
-args = vars(ap.parse_args())
+import numpy as np
 
 # Create a VideoCapture object
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0)
 
 # Check if camera opened successfully
 if (cap.isOpened() == False): 
@@ -25,49 +10,16 @@ if (cap.isOpened() == False):
 
 # Default resolutions of the frame are obtained.The default resolutions are system dependent.
 # We convert the resolutions from float to integer.
-frame_width = int(720)
-frame_height = int(720)
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
 
 # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
 out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-
-# open the output CSV file for writing and initialize the set of
-# barcodes found thus far
-csv = open(args["output"], "w")
-found = set()
 
 while(True):
   ret, frame = cap.read()
 
   if ret == True: 
-    frame = imutils.resize(frame, frame_width)
-    
-    # find the barcodes in the frame and decode each of the barcodes
-    barcodes = pyzbar.decode(frame)
-
-            # loop over the detected barcodes
-    for barcode in barcodes:
-            # extract the bounding box location of the barcode and draw
-            # the bounding box surrounding the barcode on the image
-        (x, y, w, h) = barcode.rect
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            # the barcode data is a bytes object so if we want to draw it
-            # on our output image we need to convert it to a string first
-        barcodeData = barcode.data.decode("utf-8")
-        barcodeType = barcode.type
-            # draw the barcode data and barcode type on the image
-        text = "{} ({})".format(barcodeData, barcodeType)
-        center = "%d , %d" % ((x+w)/2,(y+h)/2)
-        text = text + center
-        cv2.putText(frame, text, (x, y - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            # if the barcode text is currently not in our CSV file, write
-            # the timestamp + barcode to disk and update the set
-        if barcodeData not in found:
-            csv.write("{},{}\n".format(datetime.datetime.now(),
-                barcodeData,x,w,y,h))
-            csv.flush()
-            found.add(barcodeData)
     
     # Write the frame into the file 'output.avi'
     out.write(frame)
@@ -84,7 +36,6 @@ while(True):
     break  
 
 # When everything done, release the video capture and video write objects
-csv.close()
 cap.release()
 out.release()
 
